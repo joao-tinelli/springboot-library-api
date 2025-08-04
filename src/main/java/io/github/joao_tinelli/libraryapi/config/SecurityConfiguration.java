ae24e3b2c8238d2233bdc23827d3df5dc29bf262
Joao Tinelli <joao_tinelli@outlook.com>
2025-08-04 12:17:59 -0300

    [Spring Security]  usuarios criados  em memoria e permissoes definidas

--- src/main/java/io/github/joao_tinelli/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/joao_tinelli/libraryapi/config/SecurityConfiguration.java
@@ -2,10 +2,17 @@ package io.github.joao_tinelli.libraryapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,13 +23,42 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                //.httpBasic(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
                 .formLogin(configurer -> {
                     configurer.loginPage("/login").permitAll();
                 })
                 .authorizeHttpRequests(authorize -> {
-                  authorize.anyRequest().authenticated();
+                    authorize.requestMatchers("/login").permitAll();
+                    authorize.requestMatchers(HttpMethod.POST, "/autores/**").hasRole("ADMIN"); // apenas ADMINs podem salvar um autor
+                    authorize.requestMatchers(HttpMethod.DELETE, "/autores/**").hasRole("ADMIN"); // apenas ADMINs podem deletar um autor
+                    authorize.requestMatchers(HttpMethod.PUT, "/autores/**").hasRole("ADMIN"); // apenas ADMINs podem atualizar um autor
+                    authorize.requestMatchers(HttpMethod.GET, "/autores/**").hasAnyRole("USER, ADMIN"); // tanto USERs quanto ADMINs podem visualizar um autor
+                    authorize.requestMatchers("/livros/**").hasAnyRole("USER", "ADMIN");
+
+                    authorize.anyRequest().authenticated(); // para as demais requisicoes, qualquer usuario autenticado podera fazer
                 })
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        UserDetails user1 = User.builder()
+                .username("usuario")
+                .password(encoder.encode("123"))
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("admin")
+                .password(encoder.encode("321"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
