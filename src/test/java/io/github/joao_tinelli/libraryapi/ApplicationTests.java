a7ce36d69d844e01ba2b2297c7ee28b8375c2166
Joao Tinelli <joao_tinelli@outlook.com>
2026-04-14 15:41:53 -0300

    Fix: ajusta contexto de testes com o Testcontainers

--- src/test/java/io/github/joao_tinelli/libraryapi/ApplicationTests.java
+++ src/test/java/io/github/joao_tinelli/libraryapi/ApplicationTests.java
@@ -1,10 +1,11 @@
 package io.github.joao_tinelli.libraryapi;
 
+import io.github.joao_tinelli.libraryapi.integration.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class ApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
